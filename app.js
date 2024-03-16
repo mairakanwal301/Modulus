f86f6d64ad71569937e2226f6394ca282b91cf66
mairakanwal301 <159882309+mairakanwal301@users.noreply.github.com>
2024-03-16 05:49:00 -0700

    Modulus
    I am a student of governor sindh IT initiative and I m a begginer and I have no programming background but alhamdulillah my concepts are clear now and doing self learning also from other platforms..

---
+++ app.js
@@ -0,0 +1,6 @@
+// MODULUS:
+var apples = 5;
+var bags = 2;
+var reminder = apples % bags;
+//console.log(reminder);
+console.log(5 % 2);
---
+++ app.ts
@@ -0,0 +1,6 @@
+// MODULUS:
+let apples:number = 5;
+let bags: number = 2;
+let reminder = apples % bags ;
+//console.log(reminder);
+console.log(5 % 2);
